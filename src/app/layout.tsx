e21410026346e8b795ae62967865a7e3383d7a88
Hieu <khoanono963@gmail.com>
2025-02-24 20:18:59 +0700

    up

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>)  {
+  
+  return (
+    <html lang="en">
+          <body
+          >
+          <div>{children}</div>
+        
+          </body>
+        </html>)
+    
+}
